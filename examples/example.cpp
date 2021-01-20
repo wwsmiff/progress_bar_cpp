da524bcf99a0a42ca334af1b150244f14cce9fd3
Llawliet0872 <arnav0872@gmail.com>
2021-01-20 13:25:49 +0530

    example.cpp uses getSize()

--- examples/example.cpp
+++ examples/example.cpp
@@ -8,18 +8,18 @@ int main()
 	ProgressBar foo('.', '#', 30); 
 
 	foo.done = 0;
-	foo.todo = 28;
+	foo.todo = foo.getSize();
 
 	std::cout << "Doing a task" << std::endl;
 
-	for(int i = 0; i < 29; i++)
+	for(int i = 0; i < foo.getSize(); i++)
 	{
+ 		foo.done++;		
 		usleep(0.1 * micros); // A delay of 0.1 second(s)
 		foo.fillUp();
 		foo.displayPercentage();
 		std::cout << " | ";
-		foo.displayTasksDone();
-		foo.done++;
+		foo.displayTasksDone();	
 	}
 
 	foo.end();
